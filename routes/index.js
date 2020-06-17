e0ec50dd184e702765f4b951b441e3c9e148f907
zzpapy <gregory.pace@hotmail.fr>
2020-06-17 16:17:23 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+ const tab = [
+   "greg", "prag","laurent"
+ ]
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Toto' });
+});
+
+/* GET home page. */
+router.get('/toto/:id', function(req, res, next) {
+  let id = req.params.id
+  console.log(id)
+  res.render('page1', { 
+    title: 'tutu',
+  "nom":tab[id]+id });
+});
+
+module.exports = router;
